0b809252f53db8e6160b97a6907819936640bea8
Cuong Dang <cuong.dang@tyme.com>
2023-02-27 21:40:07 +0700

    Initial commit

---
+++ crawl_data/spiders/companies_spider.py
@@ -0,0 +1,30 @@
+import scrapy
+import time
+
+class CompaniesSpider(scrapy.Spider):
+    """This spider wil crawl all the company link available in itviec and save it
+    to a json line file.
+    """
+    name = "companies"
+    start_urls = [
+        'https://itviec.com/companies',
+    ]
+
+    def parse(self, response):
+        all_companies = response.xpath("//div[@class='first-group companies']/a[@class='featured-company']/@href").getall()
+        for company_link in all_companies:
+            relative_link = '/'.join(company_link.split('/') [:-1])
+            company_name = company_link.split('/') [-2]
+            absolute_link = response.urljoin(relative_link)
+            yield {'company_name': company_name, 'url': absolute_link }
+            # adding 1 seconds time delay
+            time.sleep(1)
+
+        next_page = response.xpath("//a[@class='more-jobs-link more-company']/@href").get()
+        # next_page now has the form of '/companies?page=2' or None
+
+        if next_page is not None:
+            # makes absolute url
+            next_page = response.urljoin(next_page)
+            yield scrapy.Request(next_page, callback = self.parse)
+
---
+++ crawl_data/spiders/job_spider.py
@@ -0,0 +1,53 @@
+import scrapy
+import json
+import time
+
+class JobSpider(scrapy.Spider):
+    """This spider will crawl all relevant information of all jobs for all 
+    company in companies_info.jl
+    
+    A job will have the following information:
+        company_name: str: the company name that offer this job
+        job_name: str
+        tag_list: str: tag list of the job
+        location: str: location of the job
+        three_reasons: str: Top three reasons to join us
+        description: str
+        requirements: str: your skills and Experience
+        why_join: str: why you'll love working here
+
+    
+    """
+    name = 'job_info'
+
+    def start_requests(self, ):
+        
+        company_jobs = {}
+        file_path = '.\itviec_companies_info.jl'
+        with open(file_path, 'r', encoding = 'utf-8') as f:
+            for line in f:
+                company_info = json.loads(line)
+                if company_info['company_name']:
+                    company_jobs[company_info['company_name']] = company_info['jobs']
+
+        for company_name, jobs in company_jobs.items():
+            for job_name, job_url in jobs.items():
+                yield scrapy.Request(job_url,
+                    cb_kwargs = {'company_name': company_name, 'job_name': job_name},
+                    callback= self.parse)
+
+    def parse(self, response, company_name, job_name):
+        job_info = {}
+
+        job_info['company_name'] = company_name
+        job_info['job_name'] = job_name
+        job_info['tag_list'] = '\n'.join(response.xpath("//div[@class='job-details__tag-list']/a/span/text()").getall())
+        job_info['location'] = response.xpath("//div[@class='job-details__overview']//div[@class='svg-icon__text']//span/text()").get()
+        job_info['three_reasons'] = '\n'.join(response.xpath("//div[@class='job-details__top-reason-to-join-us']//li/text()").getall())
+        job_info['description'] = '\n'.join(response.xpath("//div[@class='job-details__paragraph']").getall())
+        # adding 1 seconds time delay
+        time.sleep(1)
+        
+        yield job_info
+
+        
